26ebd18af48c61d65fa4b048437fa911c9e1ecd3
BrookeGibbons <21301367@student.uwa.edu.au>
2019-03-29 13:15:47 +0800

    commit

--- R/length.sp.to.spp.R
+++ R/length.sp.to.spp.R
@@ -12,26 +12,20 @@
 #'
 #' @export
 length.sp.to.spp<-function(dat,sp.list,return.changes=FALSE){
-  
   length.number<-dat%>%
     group_by(campaignid,sample,family,genus,species)%>%
     summarise(total=sum(number))%>%
     ungroup()%>%
     mutate(new.species=ifelse(species%in%sp.list,"spp",as.character(species)))
-  
   length.max<-length.number%>%
     group_by(campaignid,sample,family,genus,new.species)%>%
     slice(which.max(total))%>%
     left_join(.,length.number) # only select the ones with the highest total
-  
   dat.spp<-semi_join(dat,length.max, by = c("campaignid", "sample", "family", "genus", "species")) # only selects ones that had the highest
-  
   dat.spp<-dat.spp%>%
     dplyr::mutate(species=ifelse(species%in%sp.list,"spp",as.character(species)))
-  
   if(return.changes==TRUE){length.taxa.replaced.by.spp<<-anti_join(dat,dat.spp,by=c("campaignid","sample","genus","species"))%>%
     distinct(campaignid,sample,family,genus,species)
   } 
-  
   return(dat.spp)
 }
