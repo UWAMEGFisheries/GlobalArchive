3233c0188e111aa535cfb76eb39b478883f61ed0
BrookeGibbons <21301367@student.uwa.edu.au>
2019-03-29 14:13:29 +0800

    try to save the report

--- R/synonyms.R
+++ R/synonyms.R
@@ -11,15 +11,21 @@
 #' change.synonyms()
 #'
 #' @export
-change.synonyms<-function(dat,return.changes=FALSE){
+change.synonyms<-function(dat,return.changes=FALSE,save.report=FALSE){
 dat.spp<-left_join(dat,synonyms,by=c("family","genus","species"))%>%
     mutate(genus=ifelse(!is.na(genus_correct),genus_correct,genus))%>%
     mutate(species=ifelse(!is.na(species_correct),species_correct,species))%>%
     mutate(family=ifelse(!is.na(family_correct),family_correct,family))%>%
     select(-c(family_correct,genus_correct,species_correct))
  
-if(return.changes==TRUE){taxa.replaced.by.synonym<<-anti_join(dat,dat.spp,by=c("campaignid","sample","genus","species"))%>%
-  distinct(campaignid,sample,family,genus,species)
+if(return.changes==TRUE){taxa.replaced.by.synonym<<-left_join(dat,synonyms,by=c("family","genus","species"))%>% 
+  filter(!is.na(genus_correct))%>%
+  select(campaignid,family,genus,species,family_correct,genus_correct,species_correct)%>%
+  distinct()
+} 
+
+if(save.report==TRUE){setwd(error.dir)
+  write.csv(taxa.replaced.by.synonym,paste(study,deparse(substitute(dat)),"taxa.replaced.by.synonym.csv",sep = "."))
 } 
 
 return(dat.spp)
