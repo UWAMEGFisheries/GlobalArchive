6de6bf6fb8d92f36b6507cd5875630216b7cacaa
BrookeGibbons <21301367@student.uwa.edu.au>
2019-04-30 09:20:05 +0800

    commit

--- build package.R
+++ build package.R
@@ -3,13 +3,17 @@ library(tidyr)
 library(devtools)
 library(stringr)
 
+("C:/Program Files/R/R-3.6.0/library")
+
+install.packages("rcmdcheck","H:/My Documents/R/win-library/3.6")
+
 document()
 # 
 build()
 # 
 # 
 # 
-# check()   #have to manually correct the NAMESPACE
+check()   #have to manually correct the NAMESPACE
 
 install()
 
@@ -23,5 +27,7 @@ citation("GlobalArchive")
 
 # Test
 clean_names(cars)
+
+GlobalArchive::c
 
 q<-query.pattern("sdfsd")
--- inst/url patterns.R
+++
@@ -1,7 +0,0 @@
-# Some url patterns for querying
-URL_DOMAIN <- "https://globalarchive.org"
-# URL_DOMAIN <- "http://localhost:5000"
-API_ENDPOINT_CAMPAIGN_LIST <- "/api/campaign"
-API_ENDPOINT_CAMPAIGN_DETAIL <- "/api/campaign-full/%s"
-API_ENDPOINT_CAMPAIGN_FILE <- "/api/campaign_file_file/%s"
-
